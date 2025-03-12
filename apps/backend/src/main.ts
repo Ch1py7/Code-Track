b1728a12e89aeb03dee86f206a37e00f1ed868f6
ch1py7 <garcialopezgerardo6612@gmail.com>
2025-03-11 22:07:06 -0600

    feat: enable CORS support in the main application bootstrap

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,7 +6,7 @@ import { TaskHttpModule } from './modules/task/task-http.module'
 config()
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(TaskHttpModule)
+  const app = await NestFactory.create<NestExpressApplication>(TaskHttpModule, { cors: true })
   await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap()
